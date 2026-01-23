2f55eb902b999b13ee15d7c01958fb37ee8ab733
william <will27998468@gmail.com>
2026-01-22 19:56:32 -0500

    added a sorter still errros

--- scheduling/app.py
+++ scheduling/app.py
@@ -1,54 +1,44 @@
 import streamlit as st
 import pandas as pd
-import json
-import sys
-import os
 tab1, tab2 = st.tabs(["Data", "ranker"])
 df = pd.DataFrame(pd.read_csv('m.csv'))
-
-with open("avgs.json", "r") as goy:
-    avgs = json.load(goy)
-
+import time
 extra_df = pd.DataFrame(pd.read_csv('custom.csv'))
-def update(col_num, selector, multiplier):
-    try:
-        selector_1_multiplier = float(st.session_state[multiplier])
-    except:
-        selector_1_multiplier = 1.0
-    row = {
-        f"multiplier{col_num}": multiplier,
-        f"selector{col_num}": selector}
-    add_df = pd.read_csv('custom.csv')
-    add_df = pd.concat([add_df, pd.DataFrame([row])], ignore_index=True)
-    add_df.to_csv('custom.csv', index=False)
 
+def update(s1,m1,s2,m2,s3,m3,s4,m4):
+    row = {
+        "multiplier1":m1, "selector1" :s1,
+        "multiplier2":m2, "selector2" :s2,
+        "multiplier3":m3, "selector3" :s3,
+        "multiplier4":m4, "selector4" :s4,
+}
+    extra_df = pd.read_csv('custom.csv')
+    extra_df = pd.concat([extra_df, pd.DataFrame([row])], ignore_index=True)
+    extra_df.to_csv('custom.csv', index=False)
+    
 
 max_rows = len(df[["Team_Number"]])
 
-st.data_editor(
-    df,
-    column_order=("Team_Number","Pickability","Available_Teams", "Values1", "Values2", "Values3", "Values4"),
-    num_rows= "dynamic",
-    hide_index=True,
-    disabled=["widgets"],
-    key = "chud"
-    )
-st.write(st.session_state["chud"])
 
 
 with tab1:
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
     st.write("work in progress")
 with tab2:
+
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
     label = "multiplier"
     with col1:
         if st.button("run"):
-            update(5, st.session_state["selector_1"], st.session_state["multiplier_1"])
-            update(6, st.session_state["selector_2"], st.session_state["multiplier_2"])
-            update(7, st.session_state["selector_3"], st.session_state["multiplier_3"])
-            update(8, st.session_state["selector_4"], st.session_state["multiplier_4"])
-            update(None, st.session_state)
+            update(st.session_state["selector_1"], st.session_state["multiplier_1"], 
+                   st.session_state["selector_2"], st.session_state["multiplier_2"],
+                   st.session_state["selector_3"], st.session_state["multiplier_3"],
+                   st.session_state["selector_4"], st.session_state["multiplier_4"])
+            extra_df = pd.DataFrame(pd.read_csv("custom.csv"))
+            df["Values1"]=df["Values1"].astype(object) *extra_df.iloc[-1][f"multiplier1"]
+            df["Values2"]=df["Values2"].astype(object) *extra_df.iloc[-1][f"multiplier2"]
+            df["Values3"]=df["Values3"].astype(object) *extra_df.iloc[-1][f"multiplier3"]
+            df["Values4"]=df["Values4"].astype(object) *extra_df.iloc[-1][f"multiplier4"]
 
     with col5:
         selector_1 = st.selectbox("Criteria", ["AUTO points", "Teleop Points", "Endgame Points", "Climb"], key = "selector_1")
@@ -66,3 +56,12 @@ with tab2:
     with col8:
         selector_4 = st.selectbox("Criteria", ["AUTO points", "Teleop Points", "Endgame Points", "Climb"],key = "selector_4")
         selector_4_multiplier = st.text_input(label, key = "multiplier_4")
+        
+    st.data_editor(
+    df,
+    column_order=("Team_Number","Selected","Pickability","Available_Teams", "Values1", "Values2", "Values3", "Values4"),
+    hide_index=True,
+    disabled=["widgets"],
+    key = "chud"
+    )
+
